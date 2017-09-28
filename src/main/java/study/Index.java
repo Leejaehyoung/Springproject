1494439dd2b340435b0f565c6647abb7407c8052
semi <semi@ip-192-168-0-11.ap-northeast-2.compute.internal>
2017-09-28 16:26:06 +0900

    테스트용 소스

---
+++ src/main/java/study/Index.java
@@ -0,0 +1,15 @@
+package study;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Index {
+	private static final Logger log = LoggerFactory.getLogger(Index.class);
+
+	public static void main(String[] args) {
+		
+		log.debug("Spring Project 실습");
+
+	}
+
+}
